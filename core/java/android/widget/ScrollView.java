85f74e3d5f3308fb068495c74017ca1781648fe5
PrimeDirective <activethrasher00@gmail.com>
2013-09-30 13:41:16 -0400

    Scroll: modified velocitytracker
    Velocitytracker and overscroll are called on every touch event to
    calculate if there is a scrolling/fling. Move this and the recycling
    to static voids so they are pre-optimized at compile time rather than
    after evoking the method call during runtime (JIT) to increase responsiveness.
    
    -Vanir
    
    Change-Id: Ic7351da58f29281ca2b6a788386b95b68b552390
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/widget/ScrollView.java
+++ core/java/android/widget/ScrollView.java
@@ -89,7 +89,7 @@ public class ScrollView extends FrameLayout {
 
     private final Rect mTempRect = new Rect();
     @UnsupportedAppUsage
-    private OverScroller mScroller;
+    private static OverScroller mScroller;
     /**
      * Tracks the state of the top edge glow.
      *
@@ -142,7 +142,7 @@ public class ScrollView extends FrameLayout {
      * Determines speed during touch scrolling
      */
     @UnsupportedAppUsage
-    private VelocityTracker mVelocityTracker;
+    private static VelocityTracker mVelocityTracker;
 
     /**
      * When set to true, the scroll view measure its child to make it fill the currently
@@ -567,7 +567,7 @@ public class ScrollView extends FrameLayout {
         }
     }
 
-    private void recycleVelocityTracker() {
+    private static void recycleVelocityTracker() {
         if (mVelocityTracker != null) {
             mVelocityTracker.recycle();
             mVelocityTracker = null;
