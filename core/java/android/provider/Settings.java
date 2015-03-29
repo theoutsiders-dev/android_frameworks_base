8fb673a47dfd689bcca8a071b914ad5bf5d9dcac
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    fox: disable by default
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5633,6 +5633,13 @@ public final class Settings {
          */
         public static final String SENSOR_BLOCK = "sensor_block";
 
+         /**
+          * Screenshod sound enable, This is the noise made when taking a screesnhot
+          * Defaults to 1 - sounds enabled
+          * @hide
+          */
+         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
         /**
          * Whether to show weather on lockscreen
          * @hide
