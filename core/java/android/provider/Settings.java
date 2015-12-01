54e19e800984002400f949a3281b9b3487339470
rascarlo <rascarlo@gmail.com>
2015-12-01 19:18:53 +0100

    Volume Rocker Wake [1/2]
    Change-Id: Ia32e77b4737efd5816da1b4cfb74138fc357a46b
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5294,6 +5294,12 @@ public final class Settings {
         public static final String SHOW_LOCKSCREEN_INDICATOR_DISPLAY = "show_lockscreen_indicator_display";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
