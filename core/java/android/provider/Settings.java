4cb21bf50f051755fa7590c96dac8ecfde9cd9fd
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-09 20:53:52 +0200

    SystemSensorManager: sensor block per-package [1/2]
    Evolution edits: Also add Accelerometer and Linear Acceleration sensors
    
    Change-Id: I88ece292a6ef6867577b2e9cca5ea5e2d1d471b2
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5580,6 +5580,12 @@ public final class Settings {
         private static final Validator DEVICE_FEATURE_SETTINGS_VALIDATOR = ANY_STRING_VALIDATOR;
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
