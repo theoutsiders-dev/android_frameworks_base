d32aa68795ac9fd358a2742309c39ab7284b58ef
Alex Cruz <du.alexcruz@gmail.com>
2018-02-08 15:15:14 +0100

    Disable haptic feedback on power menu items
    Power menu items without long press actions should not have haptic feedback.
    It not only makes zero sense but gives the user the impression that something
    is not working correctly. Haptic feedback is generally used to notify the user
    that an action has been triggered. If there's no action to trigger than there
    should not be haptic feedback.
    
    Change-Id: I60b4a2b58e75c1e0bfca0d271b4cdd4c83e5633d

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -321,6 +321,10 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             public boolean showBeforeProvisioning() {
                 return false;
             }
+
+            public boolean onLongPress() {
+                return false;
+            }
         };
         onAirplaneModeChanged();
 
@@ -461,7 +465,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         OnItemLongClickListener onItemLongClickListener = (parent, view, position, id) -> {
             final Action action = mAdapter.getItem(position);
             if (action instanceof LongPressAction) {
-                mDialog.dismiss();
+                //mDialog.dismiss();
                 return ((LongPressAction) action).onLongPress();
             }
             return false;
@@ -559,6 +563,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public boolean onLongPress() {
             UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
             if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+                mDialog.dismiss();
                 mWindowManagerFuncs.reboot(true);
                 return true;
             }
@@ -1336,7 +1341,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         @Override
         public boolean onLongPress() {
-            return true;
+            // pass to onClick listener (onPress action)
+            return false;
         }
 
         public boolean isEnabled() {
