3f97cf8a5902b9d5b6635abb811a27423df88495
maxwen <max.weninger@gmail.com>
2018-04-14 08:45:19 -0600

    base: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    Bootleggers (@eldainosor) edit: Let's apply to use Android Go layout too, and also allow to drag the windows on non-go builds
    
    Change-Id: I5ced8171be9f01fceb44be4a467265066ba8a884
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1229,6 +1229,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_ROCKER_WAKE), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RECENTS_LAYOUT_STYLE), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
