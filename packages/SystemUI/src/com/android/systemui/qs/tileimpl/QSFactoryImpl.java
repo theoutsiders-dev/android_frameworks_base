bf47f6dd783a3b1f9826c91b1cdc4a56353d680c
PMS22 <prathams99@rediff.com>
2018-09-16 01:25:44 +0530

    SystemUI: FireHound Gaming [1/4]
    [2/4] : packages/apps/Settings
    [3/4] : packages/apps/Extensions
    [4/4] : packages/apps/Dialer
    
    Enabling the tile will :-
    - Show a "First-click" informative dialog
    - Change ringer mode to vibrate
    - Increase the media volume to max
    - Disable Heads Up
    - Turn off Hardware keys
    
    - Turning off the tile will change all the values back to default
    - I haven't enabled Expanded desktop as I dont think any game runs
      in non expanded mode, so better to keep it clean.
    - Duplicate the SystemSetting class from SecureSetting one grabbing
      the System Settings string :-
      https://github.com/FireHound/android_frameworks_base/blob/o8.1/packages/SystemUI/src/com/android/systemui/qs/SecureSetting.java
    
    * NEW CHANGES :- *
    
    1) User can select apps for which gaming mode will be turned on automtically.
    2) Any incoming call will be handled in the background if answered
       and no InCallUI will be shown.
    3) All notifications from the Dialer app will be shown. There won't be a full-screen
       call pick up screen on incoming calls.
    4) The tile will save the user-defined Settings state in a dummy Settings.System string
       and the same value will be restored back when tile is disabled. So now no users settings will be overrided.
    5) Added "Dynamic Gaming mode". The tile will be enabled automatically if the launched
       app is a game.
    6) Added "Lock brightness" thing. Brightness mode will be changed to manual one.
    7) Added a long press intent to open Gaming mode Settings.
    8) The "user defined" ringer state and media volume will be restored once gaming mode is turned off.
    
    Credits :-
    1) @ezio84 - storing user settings in mock Settings.
    2) @ishubhamsingh - forward port to pie and improve dynamic gaming mode
    
    Change-Id: I4d1a34be570b54e5dbd8b0099952bce1773b00e6
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -113,6 +114,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ImmersiveTile(mHost);
             case "font":
                 return new FontTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
