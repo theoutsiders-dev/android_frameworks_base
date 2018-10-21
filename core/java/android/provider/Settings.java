7d5d4f84fdd3a68cde1144996bd40bf3c3d1f34b
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-21 14:01:48 +0530

    Lock Clock & Date Font Size [1/2]
    All thanks to brother eldainosor (Bootleggers ROMS). This goes in his name.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4599,6 +4599,20 @@ public final class Settings {
         public static final String LOCK_DATE_FONTS = "lock_date_fonts";
 
         /**
+         * Change font size for the system lockscreen date
+         *
+         * @hide
+         */
+        public static final String LOCKDATE_FONT_SIZE= "lockdate_font_size";
+
+        /**
+         * Change font size for the system lockscreen clock widget
+         *
+         * @hide
+         */
+        public static final String LOCKCLOCK_FONT_SIZE = "lockclock_font_size";
+
+        /**
          * 0 - wallpaper based
          * 1 - force light
          * 2 - force dark
