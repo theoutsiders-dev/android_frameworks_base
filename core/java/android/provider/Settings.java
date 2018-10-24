bcc51ae8d0cf8d42b03a290dee9c465b99eccf3b
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-25 00:43:29 +0530

    LS OwnerInfo Fonts & Size Options [1/2] SuperdroidBond:- It's done on the basis of LS Date Font & Size.
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4615,6 +4615,20 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_INFO = "lockscreen_info";
 
+	/**
+	 ** Change fonts for the system lockscreen Owner info
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCK_OWNERINFO_FONTS = "lock_ownerinfo_fonts";
+
+        /**
+	 ** Change font size for the system lockscreen owner
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCKOWNER_FONT_SIZE= "lockowner_font_size";
+
         /**
          * Select various accents to go along with system themes
          *
