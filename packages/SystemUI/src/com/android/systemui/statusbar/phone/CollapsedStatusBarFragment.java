e8c4abeaa4ca312adfc4b74acec10f99ded50686
mydongistiny <jaysonedson@gmail.com>
2018-12-23 02:14:50 -0600

    Fix NPE when loading settings
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'android.content.ContentResolver android.content.Context.getContentResolver()' on a null object reference
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.CollapsedStatusBarFragment.updateSettings(CollapsedStatusBarFragment.java:389)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.CollapsedStatusBarFragment$SettingsObserver.onChange(CollapsedStatusBarFragment.java:91)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.database.ContentObserver.onChange(ContentObserver.java:130)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.database.ContentObserver.onChange(ContentObserver.java:145)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.database.ContentObserver$NotificationRunnable.run(ContentObserver.java:216)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:873)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:193)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:6718)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:495)
    03-29 02:07:17.088  1937  1937 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:858)
    
    Steps to duplicate:
    - Remove the navbar or use a device with just hw keys
    - Remove DU logo
    - Reboot your phone
    - Turn on DU logo after you boot
    
    Change-Id: I90cc3166a26850e4f25f36c5ce063eb2a21755d3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -21,6 +21,7 @@ import static android.app.StatusBarManager.DISABLE_SYSTEM_INFO;
 import android.annotation.Nullable;
 import android.app.Fragment;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -71,6 +72,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private StatusBar mStatusBarComponent;
     private DarkIconManager mDarkIconManager;
     private View mOperatorNameFrame;
+    private ContentResolver mContentResolver;
 
     // DU Logo
     private ImageView mDULogo;
@@ -82,13 +84,13 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             super(handler);
         }
 
-         void observe() {
-            getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+        void observe() {
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO),
                     false, this, UserHandle.USER_ALL);
         }
 
-         @Override
+        @Override
         public void onChange(boolean selfChange) {
             updateSettings(true);
         }
@@ -106,6 +108,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContentResolver = getContext().getContentResolver();
         mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
@@ -394,7 +397,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void updateSettings(boolean animate) {
         mShowLogo = Settings.System.getIntForUser(
-                getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO, 0,
+                mContentResolver, Settings.System.STATUS_BAR_LOGO, 0,
                 UserHandle.USER_CURRENT) == 1;
         if (mNotificationIconAreaInner != null) {
             if (mShowLogo) {
@@ -404,6 +407,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             } else {
                 animateHiddenState(mDULogo, View.GONE, animate);
             }
-      }
-   }
+        }
+    }
 }
