41feee19cb26581db22bf130616cb320e3199695
ezio84 <brabus84@gmail.com>
2019-01-05 19:10:53 -0600

    Auto switch to a dark analog clock when using dark system theme
    - Ensure that accents change with the themes as well
    
    Change-Id: I42c4c009f6961cf1f58538ef17d9b956feb75744

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4024,6 +4024,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             mUiOffloadThread.submit(() -> {
                 unfuckBlackWhiteAccent();
                 ThemeAccentUtils.setLightDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useDarkTheme);
+                mNotificationPanel.setLockscreenClockTheme(useDarkTheme);
             });
         }
 
