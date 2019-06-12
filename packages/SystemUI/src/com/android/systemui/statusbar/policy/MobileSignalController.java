47c73bf2415a357a81437af1829d5a3e9676d90f
Malcolm Chen <refuhoo@google.com>
2019-06-11 19:48:38 -0700

    When data switches during voice call, make sure signal bar updates it.
    isDataEnabled only reflects user setting of mobile data is on or off.
    isDataCapable reflects better of whether the data could be setup or not,
    considering user settings and other restrictions and exceptions.
    
    We should use isDataCapable when deciding what to show in signal bar.
    
    When non default data SIM is in voice call, there could be an exception
    that we also setup data on that SIM.
    
    Bug: 134981077
    Test: manual
    Change-Id: I54fc4ca104cb451af847d7973fbffc7f0ed97c10
    Merged-In: I54fc4ca104cb451af847d7973fbffc7f0ed97c10
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -593,7 +593,7 @@ public class MobileSignalController extends SignalController<
     }
 
     private boolean isDataDisabled() {
-        return !mPhone.getDataEnabled(mSubscriptionInfo.getSubscriptionId());
+        return !mPhone.isDataCapable();
     }
 
     @VisibleForTesting
@@ -613,6 +613,7 @@ public class MobileSignalController extends SignalController<
         pw.println("  mSignalStrength=" + mSignalStrength + ",");
         pw.println("  mDataState=" + mDataState + ",");
         pw.println("  mDataNetType=" + mDataNetType + ",");
+        pw.println("  isDataDisabled=" + isDataDisabled() + ",");
     }
 
     class MobilePhoneStateListener extends PhoneStateListener {
