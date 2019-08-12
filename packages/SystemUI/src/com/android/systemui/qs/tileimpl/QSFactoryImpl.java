08f5cae18832a7d4cf990747c481e9c3d6784259
Alex Cruz <mazdarider23@gmail.com>
2019-08-12 21:14:39 +0700

    QS: Navigation Bar tile
    Rewrote using SystemSetting helper
    
    Change-Id: I082876b26945facbbbc06d1175964267e4c05152
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
+import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -128,6 +129,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new MusicTile(mHost);
             case "volume_panel":
                 return new VolumeTile(mHost);
+            case "navbar":
+                return new NavBarTile(mHost);
         }
 
         // Intent tiles.
