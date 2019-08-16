977395fb61e54cb57d9f7ba1e2f0e8c1279e41c1
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-08-16 10:37:18 +0700

    Weather: Use Fahrenheit on some countries, based on MCC [1/2] *also add toogle for wheather based https://github.com/crdroidandroid/android_frameworks_base/commit/91e128cf664fe30c5c7c885551f24f6c22dc55ea
    Bahamas: 364
    Palau: 552
    Belize: 702
    Cayman Islands: 346
    Micronesia: 550
    Virgin Islands, U.S.: 376
    Puerto Rico: 330
    United States, Guam: 310,311
    United States: 312
    Marshall Islands: 551
    
    Source: https://www.worldatlas.com/articles/countries-that-use-fahrenheit.html

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5634,6 +5634,12 @@ public final class Settings {
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
+         * Whether to show weather on lockscreen
+         * @hide
+         */
+        public static final String WEATHER_LOCKSCREEN_ENABLED = "weather_lockscreen_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
