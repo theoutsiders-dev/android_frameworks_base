AospExtended/platform_frameworks_base

SystemUI: refactor VoLTE icon toggle
Change-Id: I1a5771c0ba96a4c7d1a0c1eb004dd892aa7c9326
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -193,6 +193,7 @@ public class MobileSignalController extends SignalController<
 
         mapIconSets();
         updateTelephony();
+        notifyListeners();
     }
 
 
@@ -378,15 +379,6 @@ public class MobileSignalController extends SignalController<
                 && mImsManager.isNonTtyOrTtyOnVolteEnabled();
     }
 
-    private int getVolteResId() {
-        int resId = 0;
-
-        if ( mCurrentState.imsResitered && mVoLTEicon ) {
-            resId = R.drawable.ic_volte;
-        }
-        return resId;
-    }
-
     private void updateImsRegistrationState() {
         mCurrentState.imsResitered = mPhone.isImsRegistered(mSubscriptionInfo.getSubscriptionId());
         notifyListenersIfNecessary();
@@ -410,6 +402,7 @@ public class MobileSignalController extends SignalController<
                 getCurrentIconId(), contentDescription);
 
         int qsTypeIcon = 0;
+        int resId = 0;
         IconState qsIcon = null;
         String description = null;
         // Only send data sim callbacks to QS.
@@ -426,10 +419,12 @@ public class MobileSignalController extends SignalController<
                 && !mCurrentState.carrierNetworkChangeMode
                 && mCurrentState.activityOut;
         showDataIcon &= mCurrentState.isDefault || dataDisabled;
-
+        if ( mCurrentState.imsResitered && mVoLTEicon ) {
+            resId = R.drawable.ic_volte;
+        }
         int typeIcon = (showDataIcon || mConfig.alwaysShowDataRatIcon) ? icons.mDataType : 0;
         int volteIcon = mConfig.showVolteIcon && isEnhanced4gLteModeSettingEnabled()
-                ? getVolteResId() : 0;
+                ? resId : 0;
         callback.setMobileDataIndicators(statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 activityIn, activityOut, volteIcon, dataContentDescription, description, icons.mIsWide,
                 mSubscriptionInfo.getSubscriptionId(), mCurrentState.roaming);

theoutsiders-dev/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -181,6 +181,7 @@ public class MobileSignalController extends SignalController<
                 UserHandle.USER_CURRENT) == 1;
         mapIconSets();
         updateTelephony();
+        notifyListeners();
     }
 
     public void setConfiguration(Config config) {
@@ -361,15 +362,6 @@ public class MobileSignalController extends SignalController<
                 && mImsManager.isNonTtyOrTtyOnVolteEnabled();
     }
 
-    private int getVolteResId() {
-        int resId = 0;
-
-        if ( mCurrentState.imsResitered && mVoLTEicon ) {
-            resId = R.drawable.volte;
-        }
-        return resId;
-    }
-
     private void updateImsRegistrationState() {
         mCurrentState.imsResitered = mPhone.isImsRegistered(mSubscriptionInfo.getSubscriptionId());
         notifyListenersIfNecessary();
@@ -393,6 +385,7 @@ public class MobileSignalController extends SignalController<
                 getCurrentIconId(), contentDescription);
 
         int qsTypeIcon = 0;
+        int resId = 0;
         IconState qsIcon = null;
         String description = null;
         // Only send data sim callbacks to QS.
@@ -409,10 +402,12 @@ public class MobileSignalController extends SignalController<
                 && !mCurrentState.carrierNetworkChangeMode
                 && mCurrentState.activityOut;
         showDataIcon &= mCurrentState.isDefault || dataDisabled;
-
+        if ( mCurrentState.imsResitered && mVoLTEicon ) {
+            resId = R.drawable.volte;
+        }
         int typeIcon = (showDataIcon || mConfig.alwaysShowDataRatIcon) ? icons.mDataType : 0;
         int volteIcon = mConfig.showVolteIcon && isEnhanced4gLteModeSettingEnabled()
-                ? getVolteResId() : 0;
+                ? resId : 0;
         callback.setMobileDataIndicators(statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 activityIn, activityOut, volteIcon, dataContentDescription, description, icons.mIsWide,
                 mSubscriptionInfo.getSubscriptionId(), mCurrentState.roaming);