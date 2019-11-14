28f67b6c82a3a7e87475190d31289ab79bf95f26
Stallix <team.evolutionx.rom@gmail.com>
2019-11-14 07:33:26 +0000

    Permissions: Grant Google Recorder perms
    Change-Id: Ib8659c18815f63a51b64d02c1d9ce40c4c5f6b9d
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -834,6 +834,9 @@ public final class DefaultPermissionGrantPolicy {
         // Google dialer
         grantSystemFixedPermissionsToSystemPackage("com.google.android.dialer", userId, PHONE_PERMISSIONS,
                 CONTACTS_PERMISSIONS, SMS_PERMISSIONS);
+
+        // Google Recorder
+        grantSystemFixedPermissionsToSystemPackage("com.google.android.apps.recorder", userId, MICROPHONE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
