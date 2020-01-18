ed2b58c8e2d463fcd143695ae74766a651afc2c3
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-18 11:18:17 +0700

    Revert "RecordingService: Use existing screenrecord icon"
    This reverts commit c5a1ae8d45c0bad011955cbfb38aac6c47b01894.

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -363,7 +363,7 @@ public class RecordingService extends Service {
                                 PendingIntent.FLAG_UPDATE_CURRENT))
                         .build(),
                 new Notification.Action.Builder(
-                        Icon.createWithResource(this, R.drawable.ic_screen_recorder),
+                        Icon.createWithResource(this, R.drawable.ic_android),
                         getResources().getString(R.string.screenrecord_cancel_label),
                         PendingIntent
                                 .getService(this, REQUEST_CODE, getCancelIntent(this),
@@ -378,7 +378,7 @@ public class RecordingService extends Service {
                 .setDataAndType(uri, "video/mp4");
 
         Notification.Action shareAction = new Notification.Action.Builder(
-                Icon.createWithResource(this, R.drawable.ic_screen_recorder),
+                Icon.createWithResource(this, R.drawable.ic_android),
                 getResources().getString(R.string.screenrecord_share_label),
                 PendingIntent.getService(
                         this,
@@ -388,7 +388,7 @@ public class RecordingService extends Service {
                 .build();
 
         Notification.Action deleteAction = new Notification.Action.Builder(
-                Icon.createWithResource(this, R.drawable.ic_screen_recorder),
+                Icon.createWithResource(this, R.drawable.ic_android),
                 getResources().getString(R.string.screenrecord_delete_label),
                 PendingIntent.getService(
                         this,
@@ -398,7 +398,7 @@ public class RecordingService extends Service {
                 .build();
 
         Notification.Builder builder = new Notification.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_screen_recorder)
+                .setSmallIcon(R.drawable.ic_android)
                 .setContentTitle(getResources().getString(R.string.screenrecord_name))
                 .setContentText(getResources().getString(R.string.screenrecord_save_message))
                 .setContentIntent(PendingIntent.getActivity(
